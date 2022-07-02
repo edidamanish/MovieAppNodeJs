2b738bacb9f1fd3f95f8b2c28a2db5f31ad2fde9
Manish Edida <edidamanish@gmail.com>
2022-07-03 02:19:01 +0530

    Created hashed Password added email username check

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,32 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
-  //Validation of Data
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //Need to check if email or username already there
-
-
-  
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send("Email already exists");
+
+    const userNameExist = await User.findOne({ username: req.body.username });
+    if (userNameExist) return res.status(400).send("Username already exists");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: savedUser._id });
   } catch (err) {
-    res.send(400).send(err);
+    res.status(400).send(err);
   }
 });
 
