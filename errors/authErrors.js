6b92d49c0641c142166fd101eaa12a031b01ce6a
Manish Edida <edidamanish@gmail.com>
2022-07-08 00:07:09 +0530

    refactored error code structure

---
+++ errors/authErrors.js
@@ -0,0 +1,20 @@
+module.exports = {  
+	EMAIL_ALREADY_EXISTS: {
+		message: 'email already exists',
+		code: 'auth-001'
+	},
+	USERNAME_ALREADY_EXISTS: {
+		message: 'username already exists',
+		code: 'auth-002'
+	},
+	INCORRECT_USERNAME: {
+		message: 'username is incorrect',
+		code: 'auth-003'
+	},
+	INCORRECT_PASSWORD: {
+		message: 'password is incorrect',
+		code: 'auth-004'
+	},
+	GENERIC_ERROR_MESSAGE: 'something went wrong',
+	GENERIC_ERROR_CODE: 'gen-001'
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,14 @@ const router = require('express').Router()
 const User = require('../model/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const {
+	EMAIL_ALREADY_EXISTS,
+	USERNAME_ALREADY_EXISTS,
+	INCORRECT_USERNAME,
+	INCORRECT_PASSWORD,
+	GENERIC_ERROR_MESSAGE,
+	GENERIC_ERROR_CODE
+} = require('../errors/authErrors')
 const { registerValidation, loginValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
@@ -10,13 +18,12 @@ router.post('/register', async (req, res) => {
 
 	try {
 		const emailExist = await User.findOne({ email: req.body.email })
-		if (emailExist) return res.status(400).send('Email already exists')
+		if (emailExist) return res.status(400).send(EMAIL_ALREADY_EXISTS)
 
 		const userNameExist = await User.findOne({
 			username: req.body.username
 		})
-		if (userNameExist)
-			return res.status(400).send('Username already exists')
+		if (userNameExist) return res.status(400).send(USERNAME_ALREADY_EXISTS)
 
 		const salt = await bcrypt.genSalt(10)
 		const hashedPassword = await bcrypt.hash(req.body.password, salt)
@@ -32,7 +39,10 @@ router.post('/register', async (req, res) => {
 
 		return res.send({ username: user.username, authToken: token })
 	} catch (err) {
-		return res.status(400).send(err)
+		return res.status(400).send({
+			message: GENERIC_ERROR_MESSAGE,
+			code: err?.code ?? GENERIC_ERROR_CODE
+		})
 	}
 })
 
@@ -42,16 +52,18 @@ router.post('/login', async (req, res) => {
 
 	try {
 		const user = await User.findOne({ username: req.body.username })
-		if (!user) return res.status(400).send('Username or password is wrong')
+		if (!user) return res.status(400).send(INCORRECT_USERNAME)
 
 		const validPass = await bcrypt.compare(req.body.password, user.password)
-		if (!validPass)
-			return res.status(400).send('Username or password is wrong')
+		if (!validPass) return res.status(400).send(INCORRECT_PASSWORD)
 
 		const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
 		return res.send({ username: user.username, authToken: token })
 	} catch (err) {
-		return res.status(400).send(err)
+		return res.status(400).send({
+			message: GENERIC_ERROR_MESSAGE,
+			code: err?.code ?? GENERIC_ERROR_CODE
+		})
 	}
 })
 
